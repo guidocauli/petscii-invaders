126ea35c879727dbc6e859659e737f62cb9f7e8e
guidocauli <96411424+guidocauli@users.noreply.github.com>
2022-03-18 22:16:08 +0100

    Update gfx.c

--- gfx.c
+++ gfx.c
@@ -1,3 +1,14 @@
+/*
+	PETSCII Invaders by Guido Cauli 2022
+	Copyright 2022 Guido Cauli <guido.cauli@gmail.com>
+
+	Quest'opera è stata rilasciata con licenza Creative Commons Attribuzione - Non commerciale - Non opere derivate 4.0 Internazionale. 
+	Per leggere una copia della licenza visita il sito web http://creativecommons.org/licenses/by-nc-nd/4.0/ o spedisci una lettera a
+	Creative Commons, PO Box 1866, Mountain View, CA 94042, USA.
+
+*/
+
+
 unsigned char frame0000[]={// border,bg,chars,colors
 0,0,
 32,32,32,32,32,32,32,32,32,32,32,32,32,32,32,32,32,32,32,32,32,32,32,32,32,32,32,32,32,32,32,32,32,32,32,32,32,32,32,32,
